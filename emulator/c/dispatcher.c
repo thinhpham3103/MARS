fe8633a7a1a04811582221886fbb1d4bfdc72e1e
Tom Brostrom <noemail>
2022-05-12 12:45:21 -0400

    added cbor_vget

--- emulator/c/dispatcher.c
+++ emulator/c/dispatcher.c
@@ -7,6 +7,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include "mars.h"
+#define MARS_RC_IO 99 // TODO add I/O or parsing RC
 #include "../tinycbor/src/cbor.h"
 
 extern bool failure;
@@ -23,6 +24,51 @@ typeof(len) i;
     printf("\n");
 }
 
+// cbor_vget() pulls multiple parameters from CBOR iterator
+#include <stdarg.h>
+CborError cbor_vget(CborValue *it, const char *ptype, ...)
+{
+    va_list ap;
+    bool *bp;   // boolean pointer
+    uint8_t *xp; // byte string pointer
+    int *ip;    // int pointer
+    CborError err = CborNoError;
+    size_t *zp;
+#define va_get(Z) Z = va_arg(ap, typeof(Z))
+
+    va_start(ap, ptype);
+    while (!err && *ptype)      // walk through parameter types
+        switch (*ptype++) {
+
+        case 'b':               // boolean parameter
+            va_get(bp);
+            (   err = cbor_value_get_boolean (it, bp))
+            || (err = cbor_value_advance_fixed(it));
+            break;
+
+        case 'x':               // byte string parameter
+            va_get(xp);
+            va_get(zp);
+            err = cbor_value_is_byte_string(it)
+                ? cbor_value_copy_byte_string (it, xp, zp, it)
+                : CborUnknownError;
+            if (err)
+                *zp = 0;
+            break;
+
+        case 'i':               // int parameter
+            va_get(ip);
+            (   err = cbor_value_get_int (it, ip))
+            || (err = cbor_value_advance_fixed(it));
+            break;
+        default:
+            err = CborUnknownError;
+        }
+    va_end(ap);
+    if (err) printf("VGET err %d\n", err);
+    return err;
+}
+
 
 // Extract MARS command parameters from CBOR iterator "it",
 // call the selected MARS_ command, and marshall the results
@@ -34,35 +80,33 @@ uint8_t inblob[1024], outblob[1024];
 uint8_t dig[PROFILE_LEN_DIGEST];    // TODO: consolidate the variables to reuse "hw" registers
 uint8_t key[PROFILE_LEN_KSYM];
 uint8_t sig[PROFILE_LEN_SIGN];
+uint8_t ctx[PROFILE_LEN_DIGEST];
 int err;
 unsigned int cmdcode;
 unsigned int pt, index;
-size_t bslen, bslen2;   // byte string length
+size_t xlen1, xlen2, xlen3;   // byte string length
 bool fullTest;
 uint16_t cap;
 // sizeof(uint32_t) > sizeof(int) ??
 unsigned int regsel; // type required by cbor
 
-    if (cbor_value_get_int (it, &cmdcode)
-                || cbor_value_advance_fixed(it))
-        rc = MARS_RC_BUFFER;
-    else if (failure && (cmdcode != MARS_CC_CapabilityGet)) {
+    if (cbor_vget(it, "i", &cmdcode))
+        rc = MARS_RC_IO;
+    else if (failure && (cmdcode != MARS_CC_CapabilityGet))
         rc = MARS_RC_FAILURE;
-    }
     else switch (cmdcode) {
+
         case MARS_CC_SelfTest:
-        if (cbor_value_get_boolean (it, &fullTest))
-            rc = MARS_RC_BUFFER;
-        else
-            rc = MARS_SelfTest(fullTest);
+        rc = cbor_vget (it, "b", &fullTest)
+            ? MARS_RC_IO
+            : MARS_SelfTest(fullTest);
         cbor_encode_int(enc, rc);
         break;
 
         case MARS_CC_CapabilityGet: // assumes cap is uint16_t or int
-        if (cbor_value_get_int (it, &pt))
-            rc = MARS_RC_BUFFER;
-        else
-            rc = MARS_CapabilityGet (pt, &cap, sizeof(cap));
+        rc = cbor_vget (it, "i", &pt)
+            ? MARS_RC_IO
+            : MARS_CapabilityGet (pt, &cap, sizeof(cap));
         cbor_encode_int(enc, rc);
         if (rc == MARS_RC_SUCCESS)
             cbor_encode_int(enc, cap);
@@ -74,15 +118,17 @@ unsigned int regsel; // type required by cbor
         break;
 
         case MARS_CC_SequenceUpdate:
+        // it'd be more efficient to use the bytes from it in-place,
+        // instead of copying to buf first
         if ( !cbor_value_is_byte_string(it)
-                || cbor_value_calculate_string_length (it, &bslen)
-                || (bslen > 100))
-            rc = MARS_RC_BUFFER;
+                || cbor_value_calculate_string_length (it, &xlen1)
+                || (xlen1 > 2048))
+            rc = MARS_RC_IO;
         else {
-            uint8_t buf[bslen];
+            uint8_t buf[xlen1];
             uint16_t outlen = 0;
-            cbor_value_copy_byte_string (it, buf, &bslen, it);
-            rc = MARS_SequenceUpdate(buf, bslen, 0, &outlen);
+            cbor_value_copy_byte_string (it, buf, &xlen1, it);
+            rc = MARS_SequenceUpdate(buf, xlen1, 0, &outlen);
         }
         cbor_encode_int(enc, rc);
         // hash has no output here, but other seqs might
@@ -91,71 +137,48 @@ unsigned int regsel; // type required by cbor
         break;
 
         case MARS_CC_SequenceComplete: // assumes seq is hash
-        bslen = sizeof(dig);
-        rc = MARS_SequenceComplete(dig, &bslen);
+        xlen1 = sizeof(dig);
+        rc = MARS_SequenceComplete(dig, &xlen1);
         cbor_encode_int(enc, rc);
         if (rc == MARS_RC_SUCCESS)
-            cbor_encode_byte_string(enc, dig, bslen);
+            cbor_encode_byte_string(enc, dig, xlen1);
         break;
 
-        case MARS_CC_PcrExtend:
-        if (cbor_value_get_int (it, &index)
-                || cbor_value_advance_fixed(it))
-            rc = MARS_RC_REG;
-        else if (!cbor_value_is_byte_string(it)
-                || cbor_value_calculate_string_length (it, &bslen)
-                || (bslen != PROFILE_LEN_DIGEST))
+        case MARS_CC_PcrExtend: // ( index, dig )
+        xlen1 = sizeof(dig);
+        if (cbor_vget(it, "ix", &index, &dig, &xlen1))
+            rc = MARS_RC_IO;
+        else if (xlen1 != PROFILE_LEN_DIGEST)
             rc = MARS_RC_BUFFER;
-        else {
-            uint8_t buf[bslen];
-            cbor_value_copy_byte_string (it, buf, &bslen, it);
-            rc = MARS_PcrExtend(index, buf);
-        }
+        else
+            rc = MARS_PcrExtend(index, dig);
         cbor_encode_int(enc, rc);
         break;
 
-        case MARS_CC_RegRead:
-        if (cbor_value_get_int (it, &index)
-                || cbor_value_advance_fixed(it))
-            rc = MARS_RC_REG;
-        else
-            rc = MARS_RegRead(index, dig);
+        case MARS_CC_RegRead: // ( index )
+        rc = cbor_vget(it, "i", &index)
+            ? MARS_RC_IO
+            : MARS_RegRead(index, dig);
         cbor_encode_int(enc, rc);
         if (rc == MARS_RC_SUCCESS)
             cbor_encode_byte_string(enc, dig, PROFILE_LEN_DIGEST);
         break;
 
-        case MARS_CC_Derive:
-        if (cbor_value_get_int (it, &regsel)
-                || cbor_value_advance_fixed(it))
-            rc = MARS_RC_REG;
-        else if (!cbor_value_is_byte_string(it)
-                || cbor_value_calculate_string_length (it, &bslen)
-                || (bslen > 100)) // TODO: max length
-            rc = MARS_RC_BUFFER;
-        else {
-            uint8_t ctx[bslen];
-            cbor_value_copy_byte_string (it, ctx, &bslen, it);
-            rc = MARS_Derive(regsel, ctx, bslen, key);
-        }
+        case MARS_CC_Derive: // (regSelect, ctx, ctxlen)
+        xlen1 = sizeof(ctx);
+        rc = cbor_vget(it, "ix", &regsel, &ctx, &xlen1)
+            ? MARS_RC_IO
+            : MARS_Derive(regsel, ctx, xlen1, key);
         cbor_encode_int(enc, rc);
         if (rc == MARS_RC_SUCCESS)
             cbor_encode_byte_string(enc, key, PROFILE_LEN_KSYM);
         break;
 
         case MARS_CC_DpDerive:
-        if (cbor_value_get_int (it, &regsel)
-                || cbor_value_advance_fixed(it))
-            rc = MARS_RC_REG;
-        else if (!cbor_value_is_byte_string(it)
-                || cbor_value_calculate_string_length (it, &bslen)
-                || (bslen > 100)) // TODO: max length
-            rc = MARS_RC_BUFFER;
-        else {
-            uint8_t ctx[bslen];
-            cbor_value_copy_byte_string (it, ctx, &bslen, it);
-            rc = MARS_DpDerive(regsel, ctx, bslen);
-        }
+        xlen1 = sizeof(ctx);
+        rc = cbor_vget(it, "ix", &regsel, &ctx, &xlen1)
+            ? MARS_RC_IO
+            : MARS_DpDerive(regsel, ctx, xlen1);
         cbor_encode_int(enc, rc);
         break;
 
@@ -163,49 +186,26 @@ unsigned int regsel; // type required by cbor
         cbor_encode_int(enc, MARS_RC_COMMAND);
         break;
 
-        case MARS_CC_Quote:
-        if (cbor_value_get_int (it, &regsel)
-                || cbor_value_advance_fixed(it))
-            rc = MARS_RC_REG;
-        else if (!cbor_value_is_byte_string(it)
-                || cbor_value_calculate_string_length (it, &bslen2)
-                || (bslen2 > 100)) // TODO: max length
-            rc = MARS_RC_BUFFER;
-        else {
-            uint8_t nonce[bslen2];
-            cbor_value_copy_byte_string (it, nonce, &bslen2, it);
-            if (!cbor_value_is_byte_string(it)
-                    || cbor_value_calculate_string_length (it, &bslen)
-                    || (bslen > 100)) // TODO: max length
-                rc = MARS_RC_BUFFER;
-            else {
-                uint8_t ctx[bslen];
-                cbor_value_copy_byte_string (it, ctx, &bslen, it);
-                rc = MARS_Quote(regsel, nonce, bslen2, ctx, bslen, sig);
-            }
-        }
+        case MARS_CC_Quote: // ( regSelect, nonce, nlen, ctx, ctxlen, sig )
+        xlen1 = sizeof(ctx);
+        xlen2 = sizeof(dig);  // reuse dig to hold a nonce
+        rc = cbor_vget(it, "ixx", &regsel, &dig, &xlen2, &ctx, &xlen1)
+            ? MARS_RC_IO
+            : MARS_Quote(regsel, dig, xlen2, ctx, xlen1, sig);
         cbor_encode_int(enc, rc);
         if (rc == MARS_RC_SUCCESS)
             cbor_encode_byte_string(enc, sig, PROFILE_LEN_SIGN);
         break;
 
-        case MARS_CC_Sign:
-        if (!cbor_value_is_byte_string(it)
-                || cbor_value_calculate_string_length (it, &bslen)
-                || (bslen > 100)) // TODO: max length
+        case MARS_CC_Sign: // ( ctx, ctxlen, dig, sig )
+        xlen1 = sizeof(ctx);
+        xlen2 = sizeof(dig);
+        if (cbor_vget(it, "xx", &ctx, &xlen1, &dig, &xlen2))
+            rc = MARS_RC_IO;
+        else if (xlen2 != PROFILE_LEN_DIGEST)
             rc = MARS_RC_BUFFER;
-        else {
-            uint8_t ctx[bslen];
-            cbor_value_copy_byte_string (it, ctx, &bslen, it);
-            if (!cbor_value_is_byte_string(it)
-                    || cbor_value_calculate_string_length (it, &bslen2)
-                    || (bslen2 != PROFILE_LEN_DIGEST))
-                rc = MARS_RC_BUFFER;
-            else {
-                cbor_value_copy_byte_string (it, dig, &bslen2, it);
-                rc = MARS_Sign(ctx, bslen, dig, sig);
-            }
-        }
+        else
+            rc = MARS_Sign(ctx, xlen1, dig, sig);
         cbor_encode_int(enc, rc);
         if (rc == MARS_RC_SUCCESS)
             cbor_encode_byte_string(enc, sig, PROFILE_LEN_SIGN);
@@ -213,27 +213,15 @@ unsigned int regsel; // type required by cbor
 
         bool restricted, result;
         case MARS_CC_SignatureVerify: // ( restricted, ctx, ctxlen, dig, sig )
-        // extract restricted, ctxlen (bslen)
-        if (cbor_value_get_boolean (it, &restricted)
-                || cbor_value_advance_fixed(it)
-                || !cbor_value_is_byte_string(it)
-                || cbor_value_calculate_string_length (it, &bslen)
-                || (bslen > 100)) // TODO: max length
+        xlen1 = sizeof(ctx);
+        xlen2 = sizeof(dig);
+        xlen3 = sizeof(sig);
+        if (cbor_vget(it, "bxxx", &restricted, &ctx, &xlen1, &dig, &xlen2, &sig, &xlen3 ))
+            rc = MARS_RC_IO;
+        else if (xlen2 != PROFILE_LEN_DIGEST || xlen3 != PROFILE_LEN_SIGN)
             rc = MARS_RC_BUFFER;
-        else {
-            // extract ctx, dig, sig
-            uint8_t ctx[bslen];
-            cbor_value_copy_byte_string (it, ctx, &bslen, it);
-            if (!cbor_value_is_byte_string(it)
-                    || (bslen2 = PROFILE_LEN_DIGEST, cbor_value_copy_byte_string (it, dig, &bslen2, it))
-                    || (bslen2 != PROFILE_LEN_DIGEST)
-                    || !cbor_value_is_byte_string(it)
-                    || (bslen2 = PROFILE_LEN_SIGN, cbor_value_copy_byte_string (it, sig, &bslen2, it))
-                    || (bslen2 != PROFILE_LEN_SIGN))
-                rc = MARS_RC_BUFFER;
-            else
-                rc = MARS_SignatureVerify(restricted, ctx, bslen, dig, sig, &result);
-        }
+        else
+            rc = MARS_SignatureVerify(restricted, ctx, xlen1, dig, sig, &result);
         cbor_encode_int(enc, rc);
         if (rc == MARS_RC_SUCCESS)
             cbor_encode_boolean(enc, result);
@@ -244,7 +232,9 @@ unsigned int regsel; // type required by cbor
     }
 }
 
-
+// The following is used to demonstrate the dispatcher.
+// Eventually, the dispatcher will be a stand-alone server.
+// Until then, and until an API is made, commands are generated here.
 main()
 {
 CborParser parser;
@@ -273,7 +263,7 @@ uint8_t sig[PROFILE_LEN_SIGN];
         cbor_encode_int(&array, cc);
         }
 
-    // between step1 and step2, add parameters to array
+    // between using step1 and step2, add parameters to array
 
     void step2 ()
         {
@@ -298,6 +288,8 @@ uint8_t sig[PROFILE_LEN_SIGN];
 
         // execute the command, and terminate the reply array
         dispatcher(&it, &array);
+        // TODO check for failure mode, return MARS_RC_FAILURE
+        // TODO check that IT is at end - no trailing parameters
         cbor_encoder_close_container(&out, &array);
         buflen = cbor_encoder_get_buffer_size(&out, outbuf);
 
@@ -367,9 +359,9 @@ uint8_t sig[PROFILE_LEN_SIGN];
     cbor_parser_init(outbuf, buflen, 0, &parser, &it);
     cbor_value_enter_container(&it, &it);
     cbor_value_advance_fixed(&it); // skip response code
-    size_t bslen = sizeof(sig);
-    cbor_value_copy_byte_string(&it, sig, &bslen, 0);
-    hexout("SIG", sig, bslen);
+    size_t xlen1 = sizeof(sig);
+    cbor_value_copy_byte_string(&it, sig, &xlen1, 0);
+    hexout("SIG", sig, xlen1);
 
     step1(MARS_CC_SignatureVerify); // (restricted, ctx, ctxlen, dig, sig, result)
     cbor_encode_boolean(&array, false);
