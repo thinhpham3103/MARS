24342f6f2d2977314d4608b2743132ae34668d5c
Vadim Sukhomlinov <vadim.sukhomlinov@gmail.com>
2022-08-04 22:48:02 -0700

    Update CDDL definitions to result in less verbose C code
    Added convenience macro to mars_cbor.c

--- emulator/cddl/mars_cbor.c
+++ emulator/cddl/mars_cbor.c
@@ -12,9 +12,13 @@
 // back over blob.
 void dispatcher(void *inblob, size_t inlen, void *outblob, size_t *outlen_p)
 {
-    struct mars_command_ command;
+    struct mars_command command;
     size_t len_out;
     struct mars_response response;
+#define RSP(a, b) response._mars_response_rc__##a##_Rsp._##a##_Rsp_##b
+#define DATA(a, b) command._mars_command_union__##a._##a##_##b
+#define CHOICE(a) response._mars_response_rc_choice = _mars_response_rc__##a##_Rsp
+
     uint8_t dig[PROFILE_LEN_DIGEST];
     uint8_t key[PROFILE_LEN_KSYM];
     uint8_t sig[PROFILE_LEN_SIGN];
@@ -22,45 +26,67 @@ void dispatcher(void *inblob, size_t inlen, void *outblob, size_t *outlen_p)
 
     if (cbor_decode_mars_command(inblob, inlen, &command, &len_out) != ZCBOR_SUCCESS)
         return;
-    switch (command._mars_command_choice)
+
+    // Set default response to MARS_RC_FAILURE.
+    response._mars_response_rc_choice = _mars_response_rc_union;
+    response._mars_response_rc_union_choice = MARS_RC_FAILURE;
+
+    switch (command._mars_command_union_choice)
     {
-    case _mars_command__SelfTest:
-        response._mars_response_rc._MARS_RC_choice = MARS_SelfTest(command._mars_command__SelfTest._SelfTest_full_test);
+    case MARS_CC_SelfTest:
+        response._mars_response_rc_union_choice = MARS_SelfTest(DATA(SelfTest, full_test));
         break;
-    case _mars_command__CapabilityGet:
-        response._mars_response_rc._MARS_RC_choice =
-            MARS_CapabilityGet(command._mars_command__CapabilityGet._CapabilityGet_capability_choice,
-                               &response._mars_response_data._mars_response_data__CapabilityGet_Rsp,
-                               sizeof(response._mars_response_data._mars_response_data__CapabilityGet_Rsp));
-        if (response._mars_response_rc._MARS_RC_choice == _MARS_RC__RC_SUCCESS)
-            response._mars_response_data_present = 1;
+    case MARS_CC_CapabilityGet:
+    {
+        uint16_t cap;
+        response._mars_response_rc_union_choice =
+            MARS_CapabilityGet(DATA(CapabilityGet, capability_choice),
+                               &cap, sizeof(cap));
+        if (response._mars_response_rc_union_choice == MARS_RC_SUCCESS)
+        {
+            RSP(CapabilityGet, capability_data) = cap;
+            CHOICE(CapabilityGet);
+        }
         break;
-    case _mars_command__SequenceHash:
+    }
+    case MARS_CC_SequenceHash:
+        response._mars_response_rc_union_choice = MARS_SequenceHash();
         break;
-    case _mars_command__SequenceUpdate:
+    case MARS_CC_SequenceUpdate:
+    {
+        size_t outlen = 0;
+        response._mars_response_rc_union_choice = MARS_SequenceUpdate(
+            DATA(SequenceUpdate, _binary_data.value),
+            DATA(SequenceUpdate, _binary_data.len), 0, &outlen);
+        if (response._mars_response_rc_union_choice == MARS_RC_SUCCESS)
+        {
+            CHOICE(SequenceUpdate);
+            /* no data is present */
+            RSP(SequenceUpdate, _binary_data_present) = 0;
+        }
+        break;
+    }
+    case MARS_CC_SequenceComplete:
         break;
 
-    case _mars_command__SequenceComplete:
+    case MARS_CC_PcrExtend:
         break;
 
-    case _mars_command__PcrExtend:
-        break;
-
-    case _mars_command__RegRead:
+    case MARS_CC_RegRead:
         // zcbor_bstr_encode_ptr
         break;
-    case _mars_command__Derive:
+    case MARS_CC_Derive:
         break;
-    case _mars_command__DpDerive:
+    case MARS_CC_DpDerive:
         break;
-    case _mars_command__PublicRead:
+    case MARS_CC_PublicRead:
         break;
-    case _mars_command__Quote:
+    case MARS_CC_Quote:
         break;
 
-    case _mars_command__Sign:
+    case MARS_CC_Sign:
         break;
-    case _mars_command__SignatureVerify:
+    case MARS_CC_SignatureVerify:
         break;
     }
 
